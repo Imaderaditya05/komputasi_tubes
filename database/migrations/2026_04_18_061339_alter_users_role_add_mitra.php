bd374440eabd14271104d582437f232d799d2fc5
Imaderaditya05 <imaderaditya@student.telkomuniversity.ac.id>
2026-06-11 12:26:25 +0700

    Connect database Aiven

--- database/migrations/2026_04_18_061339_alter_users_role_add_mitra.php
+++ database/migrations/2026_04_18_061339_alter_users_role_add_mitra.php
@@ -3,21 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'customer', 'seller', 'mitra'])->default('customer')->change();
-        });
+        // Kosongkan dulu
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'customer', 'seller'])->default('customer')->change();
-        });
+        // Kosongkan dulu
     }
 };
